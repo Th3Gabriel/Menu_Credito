2a5c1e7cf30ca3dab29c0c323bc1da41daf3323d
gabriel.paz <gabrielmartins1955@hotmail.com>
2024-07-23 11:54:32 -0300

    Criação de telas

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,14 @@
-var box = window.document.getElementById("box_lado_direito")
-var InputEmail = window.document.getElementById('ipt_email')
-var InputSenha = window.document.getElementById('ipt_password')
-var botao = window.document.getElementById('botao_login')
-var esqueceu_Senha = window.document.getElementById('link_EsqueceuSenha')
-var logo = window.document.getElementById('lg_Sicoob')
+var box = window.document.getElementById("box_lado_direito");
+var loginForm = window.document.getElementById('login_form');
+var registerForm = window.document.getElementById('register_form');
+let link_EsqueceuSenha = window.document.getElementById('link_EsqueceuSenha');
 
 function entrar(event) {
     event.preventDefault();  // Previne o envio do formulário
 
     box.style = 'width:100%; transition-duration: 1.5s;';
-    InputEmail.style = 'display:none';
-    InputSenha.style = 'display:none';
-    botao.style = 'display:none';
-    esqueceu_Senha.style = 'display:none';
-    logo.style = 'display:none';
+    loginForm.style = 'display:none';
+    link_EsqueceuSenha.style = 'display:none';
 
     anima = setTimeout(() => {
         box.style = 'width:0%; transition-duration: 1.5s;';
@@ -24,3 +19,29 @@ function entrar(event) {
     }, 1800); // O tempo deve ser o dobro do tempo da animação
 }
 
+function mostrarRegistro() {
+    loginForm.classList.add('hidden');
+    setTimeout(() => {
+        loginForm.style.display = 'none';
+        registerForm.style.display = 'flex';
+        registerForm.classList.remove('hidden');
+    }, 500); // Duração da animação (deve ser igual ao tempo de transição no CSS)
+}
+
+function mostrarLogin() {
+    registerForm.classList.add('hidden');
+    setTimeout(() => {
+        registerForm.style.display = 'none';
+        loginForm.style.display = 'flex';
+        loginForm.classList.remove('hidden');
+    }, 500); // Duração da animação (deve ser igual ao tempo de transição no CSS)
+}
+
+function registrar(event) {
+    event.preventDefault();  // Previne o envio do formulário de registro
+
+    // Aqui você pode adicionar a lógica para registrar o usuário
+
+    alert('Usuário registrado com sucesso!');
+    mostrarLogin();
+}
